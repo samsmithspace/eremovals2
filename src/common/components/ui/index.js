8c05342d10eb76698ee809a4e539536175d63a5e
samsmithspace <1286287511@qq.com>
2025-06-04 21:53:41 +0100

    bug fix5

---
+++ src/common/components/ui/index.js
@@ -0,0 +1,14 @@
+/**
+ * UI Components barrel export
+ * Import UI components from this single file for consistency
+ *
+ *
+ */
+
+export { default as Alert } from './Alert';
+export { default as Button } from './Button';
+export { default as FormInput } from './FormInput';
+export { default as SelectInput } from './SelectInput';
+export { default as Spinner } from './Spinner';
+export { default as Modal } from './Modal';
+
